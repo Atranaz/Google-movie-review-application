38804c71394db43eb5ce48e5f4ce46a7f1b4fcca
Zaheer Ahmad <atranaz@live.com>
2019-01-24 00:51:15 +0400

    orview is done

--- src/js/app.js
+++ src/js/app.js
@@ -2,7 +2,5 @@ require('../css/style.css');
 let cars = require("./cars.js");
 let $ = require('jquery');
 
-$.each(cars, (key,value) => {
-    $('body').append('<h1>'+cars[key].name+'</h1>');
-});
+
 
